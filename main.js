3f3b819cb20294e954b27029c77d6eafada48b5f
hallaffou1234 <mouadhallaffou@gmail.com>
2024-09-01 13:10:52 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('attive');
+   searchForm.classList.remove('active');
+   navbar.classList.remove('active');
+
+}
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+   searchForm.classList.toggle('active');
+   navbar.classList.remove('active');
+   cartItem.classList.remove('active');
+    
+}
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onlick = () =>{
+   cartItem.classList.toggle('active');
+   navbar.classList.remove('active');
+   searchForm.classList.remove('active');
+ }
+ 
+window.onscroll = () =>{
+  navbar.classList.remove('active');
+  searchForm.classList.remove('active');
+  cartItem.classList.remove('active');
+}
+               
+                 
+                    
+
+
+
+
+
+// ````
